a02fc531dcab0f655a4e421f25fb17ea439f803e
程序猿小卡 <416394284@qq.com>
2016-11-02 13:01:54 +0800

    Updates 常用/日志.md
    Auto commit by GitBook Editor

---
+++ examples/most-used-config/log.js
@@ -0,0 +1,9 @@
+var express = require('express') 
+var morgan = require('morgan') 
+var app = express() 
+
+app.use(morgan('combined')) 
+
+app.get('/', function (req, res) { 
+    res.send('hello, world!') 
+});
